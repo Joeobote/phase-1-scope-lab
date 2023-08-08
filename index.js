e57a03471fe29ec36873b275caad64a679d4aa69
JosephObote <joeobote01@gmail.com>
2023-08-08 18:03:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+   return bestCustomer
+}
+// console.log(setBestCustomer())
+function overwriteBestCustomer(){
+     bestCustomer = "maybe bob"
+     return bestCustomer
+}
+// console.log(overwriteBestCustomer())
+const leastFavoriteCustomer = "Joshua"
+
+function changeLeastFavoriteCustomer({var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+   return bestCustomer
+}
+// console.log(setBestCustomer())
+function overwriteBestCustomer(){
+     bestCustomer = "maybe bob"
+     return bestCustomer
+}
+// console.log(overwriteBestCustomer())
+const leastFavoriteCustomer = "Joshua"
+
+function changeLeastFavoriteCustomer({
+    leastFavoriteCustomer= "Tom"
+    return leastFavoriteCustomer
+
+})
+// console.log(leastFavoriteCustomer())
+
+
+
+    leastFavoriteCustomer= "Tom"
+    return leastFavoriteCustomer
+
+})
+// console.log(leastFavoriteCustomer())
+
+
+
